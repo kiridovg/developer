f2a5801e66246cdbceceb40439a4de06fcd4fdaf
Kirill Dovganyuk <kiridovg@gmail.com>
2021-07-19 17:12:18 +0300

    Add View

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,20 @@
 <header class=" d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
     <h5 class="h5 my-0 mr-auto fw-normal"><a class="p-2 text-dark" href="/">NIX</a></h5>
-    <nav class="my-2 my-md-0 me-md-3">
-        <a class="p-2 text-dark" href="/about">О нас</a>
-    </nav>
-    <a class="btn btn-outline-primary" href="/login">Вход</a>
+
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+    </div>
 </header>
