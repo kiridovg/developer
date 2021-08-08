e93fb177abbcf9bb0b8e992e1543dc490354bb00
Kirill Dovganyuk <kiridovg@gmail.com>
2021-08-08 13:29:20 +0300

    Add filters, sort, search

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
-<header class=" d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
-    <h5 class="h5 my-0 mr-auto fw-normal"><a class="p-2 text-dark" href="/">NIX</a></h5>
+<header class=" d-flex flex-column flex-md-row p-3 px-md-4 mb-3 bg-body shadow-sm">
+    <h5 class="h5"><a class="flex items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0" href="/">NIX</a></h5>
 
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="relative flex items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
