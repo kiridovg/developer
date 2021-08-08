c673c70514801691393dd2963bc3feef2e974521
Kirill Dovganyuk <kiridovg@gmail.com>
2021-08-08 13:32:29 +0300

    Add Author

---
+++ database/factories/AuthorBookFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AuthorBook;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AuthorBookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AuthorBook::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'author_id' => $this->faker->numberBetween(1,10),
+            'book_id' => $this->faker->numberBetween(1,30),
+        ];
+    }
+}
