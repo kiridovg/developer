96bfd473c30ca6f82875304e2ce71dbf7c653a99
Kirill Dovganyuk <kiridovg@gmail.com>
2021-08-08 13:30:35 +0300

    Add QueryFilter

---
+++ app/Filters/QueryFilter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+abstract class QueryFilter
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var Builder
+     */
+    protected $builder;
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @param Builder $builder
+     */
+
+    public function apply(Builder $builder)
+    {
+        $this->builder = $builder;
+
+        foreach ($this->fields() as $field => $value) {
+            if (method_exists($this, $field)) {
+                call_user_func_array([$this, $field], (array)$value);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function fields(): array
+    {
+        return array_filter(
+            array_map('trim', $this->request->all())
+        );
+    }
+}
