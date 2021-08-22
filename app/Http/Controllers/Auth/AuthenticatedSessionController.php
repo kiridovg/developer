e814d973e6a0cefc0d0443cad27568447c9db1c2
Kirill Dovganyuk <kiridovg@gmail.com>
2021-08-22 12:05:00 +0300

    Add laravel/socialite

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,14 +5,23 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Repositories\UserRepository;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticatedSessionController extends Controller
 {
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * Display the login view.
      *
@@ -54,5 +63,45 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+
+    // Google login
+    public function redirectToGoogle(): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    // Google callback
+    public function handleGoogleCallback(): RedirectResponse
+    {
+        $user = Socialite::driver('google')->user();
+
+        $this->registerOrLoginUser($user);
+
+        // Return home after login
+        return redirect()->route('dashboard');
+    }
+
+    // Facebook login
+    public function redirectToFacebook(): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    // Facebook callback
+    public function handleFacebookCallback(): RedirectResponse
+    {
+        $user = Socialite::driver('facebook')->user();
+
+        $this->registerOrLoginUser($user);
+
+        // Return home after login
+        return redirect()->route('dashboard');
+    }
+
+    protected function registerOrLoginUser($data)
+    {
+        $user = $this->userRepository->createGoogleFacebook($data);
+        Auth::login($user);
     }
 }
