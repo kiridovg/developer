99fc1b5e927ff5c884a6c556b4de275c1b8964b6
Kirill Dovganyuk <kiridovg@gmail.com>
2021-08-22 12:23:26 +0300

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\AuthorBook;
 use App\Models\Book;
 use App\Models\BookCategory;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,13 +19,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        //Book::factory(30)->create();
-        //Author::factory(10)->create();
-        //Category::factory(5)->create();
-        //AuthorBook::factory(50)->create();
-        //BookCategory::factory(50)->create();
-        //$this->call(LaratrustSeeder::class);
+        User::factory(10)->create();
+        Book::factory(30)->create();
+        Author::factory(10)->create();
+        Category::factory(5)->create();
+        AuthorBook::factory(50)->create();
+        BookCategory::factory(50)->create();
+        $this->call(LaratrustSeeder::class);
 
     }
 }
